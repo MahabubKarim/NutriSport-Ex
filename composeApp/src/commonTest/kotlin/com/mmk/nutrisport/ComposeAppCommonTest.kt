56ca7ba8459e029fe60ec9d90d0324393f4a048f
Md. Mahabub Karim <mmkarim@ulc.com>
2025-08-10 13:27:04 +0600

    Initial commit

---
+++ composeApp/src/commonTest/kotlin/com/mmk/nutrisport/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.mmk.nutrisport
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
