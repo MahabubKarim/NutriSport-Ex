abf4accd0159d45e2223b20e68b3431aac278476
mmkarim <mahabub.karim21@gmail.com>
2025-08-23 01:38:24 +0600

    Updated ProfileScreen and ProfileForm

--- feature/profile/src/commonMain/kotlin/com/nutrisport/profile/ProfileScreen.kt
+++ feature/profile/src/commonMain/kotlin/com/nutrisport/profile/ProfileScreen.kt
@@ -1,27 +1,30 @@
 package com.nutrisport.profile
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.nutrisport.shared.Resources
+import com.nutrisport.shared.Surface
 import com.nutrisport.shared.component.PrimaryButton
 import com.nutrisport.shared.component.ProfileForm
 
 @Composable
 fun ProfileScreen() {
-    Column(
-        modifier = Modifier.fillMaxWidth().padding(24.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Box(
+        modifier = Modifier
+            .background(Surface)
+            .systemBarsPadding()
     ) {
         ProfileForm(
-            modifier = Modifier.weight(1f),
-            firstName = "",
+            firstName = "Max",
             onFirstNameChange = {},
             lastName = "",
             onLastNameChange = {},
--- shared/src/commonMain/kotlin/com/nutrisport/shared/component/ProfileForm.kt
+++ shared/src/commonMain/kotlin/com/nutrisport/shared/component/ProfileForm.kt
@@ -1,13 +1,10 @@
 package com.nutrisport.shared.component
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
@@ -16,11 +13,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import com.nutrisport.shared.Country
 
 @Composable
 fun ProfileForm(
@@ -41,24 +36,29 @@ fun ProfileForm(
 ) {
     var showCountryDialog by remember { mutableStateOf(false) }
 
-   /* AnimatedVisibility(
-        visible = showCountryDialog
-    ) {
-        CountryPickerDialog(
-            country = country,
-            onDismiss = { showCountryDialog = false },
-            onConfirmClick = { selectedCountry ->
-                showCountryDialog = false
-                onCountrySelect(selectedCountry)
-            }
-        )
-    }*/
+    /* AnimatedVisibility(
+         visible = showCountryDialog
+     ) {
+         CountryPickerDialog(
+             country = country,
+             onDismiss = { showCountryDialog = false },
+             onConfirmClick = { selectedCountry ->
+                 showCountryDialog = false
+                 onCountrySelect(selectedCountry)
+             }
+         )
+     }*/
 
     Column(
         modifier = modifier
             .fillMaxSize()
-            .verticalScroll(state = rememberScrollState()),
-        verticalArrangement = Arrangement.spacedBy(12.dp)
+            .padding(
+                horizontal = 24.dp,
+                vertical = 12.dp
+            )
+            .verticalScroll(state = rememberScrollState())
+            .imePadding(),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
     ) {
         CustomTextField(
             value = firstName,
