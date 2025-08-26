8d671e51db4a42a1941674d7f5c561c9b977c8b0
mmkarim <mahabub.karim21@gmail.com>
2025-08-26 15:36:55 +0600

    Added AdminPanelScreen

--- feature/admin-panel/build.gradle.kts
+++ feature/admin-panel/build.gradle.kts
@@ -59,6 +59,7 @@ android {
     defaultConfig {
         minSdk = libs.versions.android.minSdk.get().toInt()
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
