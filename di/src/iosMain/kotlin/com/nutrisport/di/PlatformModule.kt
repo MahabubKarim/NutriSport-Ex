3b174e6158257c05f07b6327f96b29673fecf650
mmkarim <mahabub.karim21@gmail.com>
2025-08-31 15:58:27 +0600

    Updated 3 platforms PlatformModule.kt

--- di/src/iosMain/kotlin/com/nutrisport/di/PlatformModule.kt
+++ di/src/iosMain/kotlin/com/nutrisport/di/PlatformModule.kt
@@ -1,9 +1,12 @@
 package com.nutrisport.di
 
 import com.nutrisport.shared.platform.PhotoPicker
+import com.nutrisport.shared.util.IOSPreferenceUtils
+import com.nutrisport.shared.util.PreferenceUtils
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual val platformModule: Module = module {
     single { PhotoPicker() }
+    single<PreferenceUtils> { IOSPreferenceUtils() }
 }
