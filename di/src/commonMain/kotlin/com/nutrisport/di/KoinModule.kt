2d98548a6021bfd4d080e0463fae0faf344eb1b2
mmkarim <mahabub.karim21@gmail.com>
2025-08-31 15:55:31 +0600

    Updated KoinModule.kt

--- di/src/commonMain/kotlin/com/nutrisport/di/KoinModule.kt
+++ di/src/commonMain/kotlin/com/nutrisport/di/KoinModule.kt
@@ -1,22 +1,45 @@
 package com.nutrisport.di
 
+import com.mmk.kmpauth.google.GoogleAuthCredentials
+import com.mmk.kmpauth.google.GoogleAuthProvider
 import com.nutrisport.adminpanel.AdminPanelViewModel
 import com.nutrisport.auth.AuthViewModel
 import com.nutrisport.data.repository.AdminRepositoryImpl
 import com.nutrisport.data.repository.CustomerRepositoryImpl
+import com.nutrisport.data.GoogleDriveUploader
 import com.nutrisport.domain.repository.AdminRepository
 import com.nutrisport.domain.repository.CustomerRepository
 import com.nutrisport.home.HomeGraphViewModel
 import com.nutrisport.manageproduct.ManageProductViewModel
 import com.nutrisport.profile.ProfileViewModel
+import com.nutrisport.shared.util.Constants
 import org.koin.core.KoinApplication
 import org.koin.core.context.startKoin
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val sharedModule = module {
+    // Single HttpClient instance for the app
+    // single { createHttpClient() }
+
+    single {
+        GoogleAuthProvider.create(
+            credentials = GoogleAuthCredentials(serverId = Constants.WEB_CLIENT_ID)
+        )
+    }
+
+    // Drive uploader (uses platform HttpClient)
+    single { GoogleDriveUploader() }
+
+    // Repositories
     single<CustomerRepository> { CustomerRepositoryImpl() }
-    single<AdminRepository> { AdminRepositoryImpl() }
+
+    // AdminRepository requires driveUploader
+    single<AdminRepository> {
+        AdminRepositoryImpl(
+            driveUploader = get()
+        )
+    }
 
     viewModelOf(::AuthViewModel)
     viewModelOf(::HomeGraphViewModel)
